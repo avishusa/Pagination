837ee42afc9fa7d3636927a467ce6d7bc6a0888a
avish <avishusa45@gmail.com>
2024-04-27 22:52:10 -0500

    Final pagination.

--- controllers/products.js
+++ controllers/products.js
@@ -27,8 +27,14 @@ const getAllProducts = async (req, res) => {
     apiData = apiData.select(selectFix)
   }
 
+  let page=Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) ||3;
+  let skip = (page-1)*limit;
+
+  apiData=apiData.skip(skip).limit(limit);
+
   const myData=await apiData
-  res.status(200).json({ myData });
+  res.status(200).json({ myData, nbHits:myData.length });
 };
 
 const getAllProductsTesting = async (req, res) => {
